41da42ebdd08da912dd894df008897d36bfdcb03
Eng-Prud <Prudence.n826@gmai.com>
2024-03-22 13:15:00 -0700

    Push my codes on Air_Bnd_V2

--- models/city.py
+++ models/city.py
@@ -5,5 +5,11 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
+
+     Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city.
+    """
+
     state_id = ""
     name = ""
--- models/review.py
+++ models/review.py
@@ -5,6 +5,13 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """ Review classto store review information """
+
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
+    """
+
     place_id = ""
     user_id = ""
     text = ""
